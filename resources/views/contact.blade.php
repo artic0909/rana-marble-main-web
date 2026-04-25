bef5fcda2e1e9e8c06e7787ff66f5cc6be8277f6
artic0909 <alfag327@gmail.com>
2026-04-25 10:56:17 +0530

    sdsds

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -182,7 +182,7 @@
         </div>
         <div class="map-container">
             <!-- Using Makrana, Rajasthan coordinates as embed -->
-            <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d4374.417580887512!2d87.97176737529654!3d22.437919279587867!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjLCsDI2JzE2LjUiTiA4N8KwNTgnMjcuNiJF!5e1!3m2!1sen!2sin!4v1777094480746!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d4374.417580887512!2d87.97176737529654!3d22.437919279587867!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjLCsDI2JzE2LjUiTiA4N8KwNTgnMjcuNiJF!5e1!3m2!1sen!2sin!4v1777094480746!5m2!1sen!2sin" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
     </div>
 </section>
